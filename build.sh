a5903b65e9eeda926a05b727dc98fd70a91b4680
zeviberlin <zeviberlin@gmail.com>
2025-07-05 10:56:44 -0400

    Conan working

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 set -e # for better error handling
 
 mkdir -p _build
-pushd "./_build" > /dev/null # silently change into build directory
 
-cmake ..
+conan profile detect --exist-ok # first ensure that I have a conan profile
+conan install . -sbuild_type=Release -of=_build --build=missing # install from the current directory (which contains conanfile.py), \n
+# the output folder should be _build, and it should ensure that I have every package
+
+pushd "_build" > /dev/null # silently change into build directory
+
+cmake .. --preset conan-release
 cmake --build .
 
 mv ./convo ..
--- conanfile.py
+++ conanfile.py
@@ -2,13 +2,16 @@ from conan import ConanFile
 
 class Conversation(ConanFile):
     generators = ("CMakeToolchain", "CMakeDeps")
-    settings = ("os", "build_type", "arch", "compiler")
+    settings = ("os", "build_type", "arch")
 
     def requirements(self):
         self.requires("freeimage/3.18.0")
 
     def build_requirements(self):
-        self.tool_requires("cmake/[>=3.4]")
+        self.tool_requires("cmake/[>=4.0.3]")
+
+    def configure(self):
+        self.options["freeimage/*"].shared = False
 
     def layout(self):
         self.folders.generators = ""
